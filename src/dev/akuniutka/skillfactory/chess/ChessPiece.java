64d7c3e79358a95233e5ef5065adce0eae2f7f5c
Andrei Kuniutka <akuniutka@gmail.com>
2022-09-25 16:44:11 +0300

    Abstract ChessPiece class created.

---
+++ src/dev/akuniutka/skillfactory/chess/ChessPiece.java
@@ -0,0 +1,19 @@
+package dev.akuniutka.skillfactory.chess;
+
+abstract public class ChessPiece {
+
+    protected String color;
+    protected boolean check = true;
+
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+
+    abstract public String getColor();
+
+    abstract public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
+    abstract public String getSymbol();
+}
